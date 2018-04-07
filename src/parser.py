67e2d147dcd5994c00d09234902fb8c5f5ead30c
ashtonvaz710 <vaz.a@husky.neu.edu>
2018-04-07 15:54:58 -0400

    Removed vocab

--- src/parser.py
+++ src/parser.py
@@ -27,14 +27,6 @@ class Parser:
             self.clean_data(parsed_files)
         parsed_files = load_pickle(abspath(OUTPUT_DIR, 'parsed_files_clean_data.pickle'))
 
-        if not os.path.isfile(abspath(OUTPUT_DIR, 'vocab_list.pickle')):
-            self.parse_data(parsed_files)
-        parsed_files = load_pickle(abspath(OUTPUT_DIR, 'parsed_files.pickle'))
-        vocab_list = load_pickle(abspath(OUTPUT_DIR, 'vocab_list.pickle'))
-        vocab_freq_dict = load_pickle(abspath(OUTPUT_DIR, 'vocab_freq_dict.pickle'))
-        index_word_dict = load_pickle(abspath(OUTPUT_DIR, 'index_word_dict.pickle'))
-        word_index_dict = load_pickle(abspath(OUTPUT_DIR, 'word_index_dict.pickle'))
-
     def fetch_data(self):
         print('Retrieving data from Gutenberg corpus...')
         train_directory = abspath(DATASET_DIR, 'Gutenberg', 'txt')
@@ -77,44 +69,6 @@ class Parser:
         parsed_files = ' '.join(parsed_files.split(' ' + START_TOKEN + ' ' + END_TOKEN + ' '))
 
         store_pickle(parsed_files, abspath(OUTPUT_DIR, 'parsed_files_clean_data.pickle'))
-
-    def parse_data(self, parsed_files):
-        print('Parsing data...')
-
-        vocab_list = []
-        vocab_freq_dict = {}
-        index_word_dict = {}
-        word_index_dict = {}
-
-        # Append start and end tags and obtain entire vocabulary as a list
-        vocab_list.append(START_TOKEN)
-        for word in word_tokenize(parsed_files):
-            vocab_list.append(word)
-            if word in FULL_STOPS:
-                vocab_list.append(END_TOKEN)
-                vocab_list.append(START_TOKEN)
-        vocab_list.append(END_TOKEN)
-
-        # Get list of list of list of words
-        parsed_files = sent_tokenize(parsed_files)
-        for i in range(len(parsed_files)):
-            parsed_files[i] = word_tokenize(parsed_files[i])
-        print(parsed_files)
-
-        vocab_freq_dict = dict(Counter(vocab_list))
-
-        for i in range(len(vocab_list)):
-            index_word_dict[i] = vocab_list[i]
-            word_index_dict[vocab_list[i]] = i
-
-        store_pickle(parsed_files, abspath(OUTPUT_DIR, 'parsed_files.pickle'))
-        store_pickle(vocab_list, abspath(OUTPUT_DIR, 'vocab_list.pickle'))
-        store_pickle(vocab_freq_dict, abspath(OUTPUT_DIR, 'vocab_freq_dict.pickle'))
-        store_pickle(index_word_dict, abspath(OUTPUT_DIR, 'index_word_dict.pickle'))
-        store_pickle(word_index_dict, abspath(OUTPUT_DIR, 'word_index_dict.pickle'))
-
-
-
 
 if __name__ == '__main__':
     filepaths = []
