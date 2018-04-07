9e0880326f7b4c31e2a1db74fe91ad422c9d52f9
ashtonvaz710 <vaz.a@husky.neu.edu>
2018-04-07 18:59:46 -0400

    Fetches all files within dataset directory

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,5 @@
 # Library Imports
-import os
-from src.utils import store_pickle, load_pickle, abspath
+from src.utils import *
 from collections import Counter
 from src.constants import *
 
@@ -10,23 +9,30 @@ class Parser:
     def __init__(self, train_directory, lowercase=False, min_freq=0):
         self.lowercase = lowercase
         self.min_freq = min_freq
-        print('Enter init...')
+        print('START')
 
-        if not os.path.isfile(abspath(OUTPUT_DIR, DATASET_FOLDER + FETCH_DATA_PICKLE)):
+        if not os.path.isfile(abspath(OUTPUT_DIR, FETCH_DATA_PICKLE)):
             self.fetch_data(train_directory)
-        parsed_files = load_pickle(abspath(OUTPUT_DIR, DATASET_FOLDER + FETCH_DATA_PICKLE))
+        parsed_files = load_pickle(abspath(OUTPUT_DIR, FETCH_DATA_PICKLE))
 
-        if not os.path.isfile(abspath(OUTPUT_DIR, DATASET_FOLDER + CLEAN_DATA_PICKLE)):
+        if not os.path.isfile(abspath(OUTPUT_DIR, CLEAN_DATA_PICKLE)):
             self.clean_data(parsed_files)
-        parsed_files = load_pickle(abspath(OUTPUT_DIR, DATASET_FOLDER + CLEAN_DATA_PICKLE))
+        parsed_files = load_pickle(abspath(OUTPUT_DIR, CLEAN_DATA_PICKLE))
+
+        print('END')
 
     def fetch_data(self, train_directory):
         print('Retrieving data from dataset...')
-        train_file_locations = [os.path.join(train_directory, f) for f in os.listdir(train_directory)]
+        train_file_locations_list = [folder[0] for folder in os.walk(train_directory)]  # List of all folders and subfolders from train_directory
+
+        # Get all file locations into a list
+        train_file_locations = []
+        for train_directory in train_file_locations_list:
+            train_file_locations.append([abspath(train_directory, f) for f in os.listdir(train_directory) if os.path.isfile(os.path.join(train_directory, f))])
+        train_file_locations = [item for sublist in train_file_locations for item in sublist]  # Flatten out list of lists
+
         parsed_files = ''
         for file in train_file_locations:
-            # file = file.replace('\'', '\\\'') if '\'' in file else file   # Replace ' with \' in file for open(file)
-
             try:
                 with open(file, 'r', errors='ignore') as f:
                     parsed_files += f.read()
@@ -34,8 +40,7 @@ class Parser:
             except:
                 print(file)
                 continue
-
-        store_pickle(parsed_files, abspath(OUTPUT_DIR, DATASET_FOLDER + FETCH_DATA_PICKLE))
+        store_pickle(parsed_files, abspath(OUTPUT_DIR, FETCH_DATA_PICKLE))
 
     def clean_data(self, parsed_files):
         print('Cleaning data...')
@@ -60,8 +65,7 @@ class Parser:
         # Replace all blank sentences with a single space
         parsed_files = ' '.join(parsed_files.split(' ' + START_TOKEN + ' ' + END_TOKEN + ' '))
 
-        store_pickle(parsed_files, abspath(OUTPUT_DIR, DATASET_FOLDER + CLEAN_DATA_PICKLE))
+        store_pickle(parsed_files, abspath(OUTPUT_DIR, CLEAN_DATA_PICKLE))
 
 if __name__ == '__main__':
-    train_directory = abspath(DATASET_DIR, DATASET_FOLDER, 'txt')   # Specify the training directory here
-    parser = Parser(train_directory, lowercase=True, min_freq=5)
+    parser = Parser(train_directory=abspath(DATASET_DIR), lowercase=True, min_freq=5)
